13c97598a83d1e3a0001f41b4f08d379bb526f5f
Rapepol <vrgcstudio@gmail.com>
2021-12-24 21:05:28 +0700

    name + 1 2

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+#include <ctime>
+typedef long long ll;
+
+ll fibonacci(ll);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
+
+ll fibonacci(ll x){
+    if(x <= 1){
+        return x;
+    }
+    else{
+        return fibonacci(x-1) + fibonacci(x-2);
+    }
+}
+
+
